768f1ea4158336fdc0cb8c7abd1d2891ec4f3ece
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-05-26 15:29:22 +0530

    Fix: Updated script
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,9 @@
 mkdir -p ~/.ssh/
+
 echo "$PRIVATE_KEY" > ~/.ssh/key.pem
 chmod 600 ~/.ssh/key.pem
-ssh -i ~/.ssh/key.pem $VM_ADDRESS 'bash -s' < $GITHUB_WORKSPACE/scripts/update.sh
+
+echo "$VM_ADDRESS" > ~/.ssh/known_hosts
+
+ssh -i ~/.ssh/key.pem -o UserKnownHostsFile=~/.ssh/known_hosts -tt ku@$VM_ADDRESS "bash -s" < $GITHUB_WORKSPACE/scripts/update.sh
+
