139867cb8113d7f328ab22a49d2ed2d146f47185
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-05-26 16:08:25 +0530

    Feat: added favicon
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import Head from 'next/head';
 
 import '../styles/main.css';
 import { ThemeProvider } from 'theme-ui';
@@ -11,6 +12,9 @@ import Nav from '../src/components/Nav';
 export default function MyApp({ Component, pageProps }) {
     return (
         <ThemeProvider theme={theme}>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" />
+            </Head>
             <Nav />
             <Component {...pageProps} />
         </ThemeProvider>
